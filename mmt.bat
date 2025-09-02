f626d95c06642aaf3ba14b8dc37871c1c90178c7
Artem Samchuk <krutog727@gmail.com>
2025-09-02 23:26:52 +0300

    I fixed my gradle!

--- mmt.bat
+++ mmt.bat
@@ -1,11 +1,12 @@
 @echo off
 REM dev script for eazy testing
 
-cd /d %~dp0
-
-
 chcp 65001
+
+pushd %~dp0
 
 call gradlew shadowJar
 
-java -jar .\build\libs\MinecraftModpackTool-1.0-SNAPSHOT-all.jar %*
+popd
+
+java -jar %~dp0\build\libs\MinecraftModpackTool-1.0-SNAPSHOT-all.jar %*
--- mmtt.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-REM dev script for eazy testing
-REM only for already assembled jar!
-
-chcp 65001
-
-java -jar %~dp0\build\libs\MinecraftModpackTool-1.0-SNAPSHOT-all.jar %*
