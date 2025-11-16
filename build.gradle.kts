af5e38e0d23c10d38b2c30e879f85b837ac1d41a
Artem Samchuk <79054301+Bumer-32@users.noreply.github.com>
2025-11-16 12:01:04 +0200

    update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,19 +28,27 @@ tasks.jar {
 }
 
 dependencies {
+    // https://mvnrepository.com/artifact/org.slf4j/slf4j-api
     implementation("org.slf4j:slf4j-api:2.0.17")
-    implementation("ch.qos.logback:logback-classic:1.5.18")
+    // https://mvnrepository.com/artifact/ch.qos.logback/logback-classic
+    implementation("ch.qos.logback:logback-classic:1.5.21")
+    // https://mvnrepository.com/artifact/org.codehaus.janino/janino
     implementation("org.codehaus.janino:janino:3.1.12")
+    // https://mvnrepository.com/artifact/org.slf4j/jul-to-slf4j
     implementation("org.slf4j:jul-to-slf4j:2.0.17")
 
-    implementation("io.ktor:ktor-client-core:3.2.3")
-    implementation("io.ktor:ktor-client-cio:3.2.3")
+    // https://ktor.io/docs/client-create-new-application.html#add-dependencies
+    implementation("io.ktor:ktor-client-core:3.3.2")
+    implementation("io.ktor:ktor-client-cio:3.3.2")
 
+    // https://mvnrepository.com/artifact/org.jetbrains.kotlinx/kotlinx-serialization-json
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
-    implementation("com.charleskorn.kaml:kaml:0.102.0")
+    // ttps://github.com/charleskorn/kaml/releases/latest
+    implementation("com.charleskorn.kaml:kaml:0.103.0")
 
 //    implementation("org.jline:jline:3.30.0")
 
+    // https://picocli.info/
     implementation("info.picocli:picocli:4.7.7")
 }
 
